65e140616839a8da91b7d970aebe1fca0df2b95d
nyg159 <nyg159@gmail.com>
2021-11-13 12:16:23 +0900

    init

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var userlogin = require('./userpage/login');
+var todolist = require('./todopage/todo');
 
 var app = express();
 
+
+app.use(bodyParser.urlencoded({ extended:false })); 
+app.use(bodyParser.json());
+app.use('/todomain', userlogin);
+app.use('/todolist', todolist);
 
 
 // 찾을 수 없는 홈페이지에 접속 했을 때 호출 되는 404 미들웨어
---
+++ todopage/todo.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', (req, res) => {
+    
+    console.log('to do list page open');
+});
