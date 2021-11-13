795990c92ea35677785a4b634a6254fb1c133cfb
DESKTOP-ORUPFV1\MYH <fldh3369@naver.com>
2021-11-13 13:31:43 +0900

    init

--- todopage/todo.js
+++ todopage/todo.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
+//test
 router.get('/', (req, res) => {
 
-    console.log('to do list page open !');
+    console.log('to do list page open !!!!!');
 });
