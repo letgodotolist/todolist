666b864b9635263271079ecf701a1100f61cda89
nyg159 <nyg159@gmail.com>
2021-11-13 13:00:53 +0900

    init

--- todopage/todo.js
+++ todopage/todo.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    
-    console.log('to do list page open');
+
+    console.log('to do list page open !');
 });
