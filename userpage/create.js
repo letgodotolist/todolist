0371fa9a58879b632eb28f1797cfe5442beebd30
nyg159 <nyg159@gmail.com>
2021-11-18 16:20:19 +0900

    init

--- userpage/create.js
+++ userpage/create.js
@@ -13,7 +13,7 @@ router.use(bodyParser.json());
 // db에 사용자 데이터를 넣는다 (회원가입)
 router.post('/createusers', async (req, res) => {
 
-	console.log("createuser page open");
+	console.log("create user");
 
 	const { email, password, name } = req.body;
 
@@ -34,11 +34,13 @@ router.post('/createusers', async (req, res) => {
             var users = User.create({
                 email, password, name,
 
-            })
+            });
                 // 유저 생성이 완료된 경우
                 console.log('유저가 생성되었습니다.'+ req.body.email);
                 res.status(201).json(users);
                 // json으로 회원 하는 아이디 비번 이름이 json으로 안나옴 오류 
+
+            
             
 				
 		}
